338ac57779f960146442a67a3dceafcbc45abf5a
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-07-14 18:53:27 -0300

    alterações

--- psw1/gabriel-santos/atividade da pagina 18 item 4.9.4/viewRoute.php
+++ psw1/gabriel-santos/atividade da pagina 18 item 4.9.4/viewRoute.php
@@ -18,7 +18,6 @@ if (!empty($_POST['form_submit'])) {
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-    cript>
     <title>Document</title>
 </head>
 
